521fc0b94320daa59da5c64a502c0c13ddfb4e7c
paulkell <paulkell@umail.iu.edu>
2018-03-10 16:36:55 -0500

    Added C# files
    Includes functionality for web browser and gui

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URLBox = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // URLBox
+            // 
+            this.URLBox.Location = new System.Drawing.Point(517, 12);
+            this.URLBox.Name = "URLBox";
+            this.URLBox.Size = new System.Drawing.Size(176, 20);
+            this.URLBox.TabIndex = 0;
+            this.URLBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.URLBox_KeyPress);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 38);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(681, 291);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(705, 341);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.URLBox);
+            this.Name = "Form1";
+            this.Text = "KSY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox URLBox;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
